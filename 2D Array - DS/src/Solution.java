e7a9913ec2eb0ac1b0f1195c8a25b8f82b9e3bc8
IvanK <sergan@mail.ru>
2021-01-21 18:25:22 +0300

    Arrays

--- 2D Array - DS/src/Solution.java
+++ 2D Array - DS/src/Solution.java
@@ -10,8 +10,15 @@ public class Solution {
 
     // Complete the hourglassSum function below.
     static int hourglassSum(int[][] arr) {
-
-
+        int max = -45;
+        int result = 0;
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j <= 3; j++) {
+                result = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if (result > max) max = result;
+            }
+        }
+        return max;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
